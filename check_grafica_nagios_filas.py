fa4b767548a7172d5bf6e3378cb0fc737c3ecd9d
tiagogba <tiago.gba@hotmail.com>
2014-05-23 16:34:23 -0300

    Update and rename grafica_nagios_filas.py to check_grafica_nagios_filas.py

--- check_grafica_nagios_filas.py
+++ check_grafica_nagios_filas.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
-#--*--encode=utf-8--*--
-list = sys.argv[1:]
-listMsg = []
+# -*- coding: UTF-8 -*-
+
+__author__ = "Tiago Albuquerque, Marcelo Varge"
+__copyright__ = "Copyright 2014, The Cogent Project"
+__credits__ = ["Tiago Albuquerque","Matthew Wakefield"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Tiago Albuquerque"
+__email__ = "tiago.albuquerque.ext@dasa.com.br"
+__status__ = "Production"
+__homolog__ = "Python 2.6.6"
+
+
+
+
+
 
 connect('system','MotionD@','t3://10.122.18.11:7000')
 domainRuntime()
