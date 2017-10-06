7e73b6d7ad82c0c0052701b0db5e86d77a387f2d
Tiago <tiago@Elite8460p.bs.br.bsch>
2017-10-06 14:38:11 -0300

    alterei banana

--- ftp.pl
+++ ftp.pl
@@ -7,7 +7,7 @@ use Net::SFTP::Foreign;
 my $sftp = Net::SFTP::Foreign->new(
     'uolcs@10.129.88.71',
     password => 'u@lc$13',
-    more     => ['-v']
+    more     => ['--v']
 );
 
 $sftp->get('/inbox/*', '/tmp/')
